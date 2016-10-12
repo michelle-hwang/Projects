c9ead19708e0448f456849382b736b2c78f39092
michelle-hwang <mxhwang@gmail.com>
2016-10-12 17:25:58 -0400

    added try except for invalid nums

--- my-solutions/compute-pi.py
+++ my-solutions/compute-pi.py
@@ -1,5 +1,13 @@
-import sys
 
-n = sys.argv[1]
+while True:
+	try:
+		n = input("Enter number of decimal places for pi: ")
+		if (n <= 20):
+			break
+		elif (n >= 0):
+			break
+	except ValueError:
+		print("Number must be between 0 and  21.")
+
 pi = 3 + (16 / 113) # Gardner's geometric construction of pi
 print(("%." + str(n) + "f") % pi)
